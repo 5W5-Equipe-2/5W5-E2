d823700ea118c42132e63e465b50da2e7b3e08d3
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-09-12 18:52:09 -0400

    S1S2 category.php teste si un template personnalisé existe, ajout d'un template-part categorie-defaut, le template-part categorie-evenement.php affiche le nom des sous-catégories (child)

--- category.php
+++ category.php
@@ -6,33 +6,41 @@
  */
 ?>
 <?php get_header(); ?>
+
 <main class="site_main">
+    <?php
+     /****Requêtes SQL de WP****************************************************/
+    $categorie = get_queried_object();
+    $args = array(
+        'category_name' => $categorie->slug,
+        'orderby' => 'title',
+        'order' => 'ASC'
+    );
+    $query = new WP_Query($args);
+
+    /****Localiser et assigner un template-part*********************************/
+    //Assigner le modèle de template-part personnalisé 
+    $categorie_modele = locate_template('template-parts/categorie-' . $categorie->slug . '.php');
+    
+    // Utiliser le modèle par défaut si un modèle personnalisé n'existe pas
+    if (empty($categorie_modele)) {
+        $categorie_modele = locate_template('template-parts/categorie-defaut.php');
+    }
+    ?>
+
+    <!-- Affichage dans WordPress --------------------------------------------------->
+    <h2 class=""><?php echo $categorie->name ?></h2>
+    <section class="">
         <?php
-      $category = get_queried_object();
-      //requête SQL de WP
-      $args = array(
-         'category_name' => $category->slug, //slug extrait le nom de la catégorie 
-         'orderby' => 'title',
-         'order' => 'ASC'
-      );
-      $query = new WP_Query($args); //2e requête
-     
-      ?>
-<h2 class=""><?php echo $category->name ?>  </h2>
-<section class="">
-<?php
-
-      if ($query->have_posts()) :
-         
-         while ($query->have_posts()) : $query->the_post(); ?>
-            <?php  /* $category -> slug */
-            get_template_part('template-parts/categorie', $category->slug); ?>
-
-         <?php endwhile; ?>
-      <?php endif;
-      wp_reset_postdata(); ?>
-   </section>
+        if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post();
+                // Inclure le modèle personnalisé ou le modèle par défaut
+                include $categorie_modele;
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+    </section>
 </main>
-
 
 <?php get_footer(); ?>
