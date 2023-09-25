30d1580cc9351a6ab44ab3079fed0477d26908a1
Vincent Hum <111765064+Vythals@users.noreply.github.com>
2023-09-25 14:45:12 -0400

    ajout du bouton burger

--- header.php
+++ header.php
@@ -13,14 +13,20 @@
      <header class="site_entete">
      <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
       <h2><?= bloginfo('description') ?></h2>
-
+      
+      <input type="checkbox" id="chkMenu" value="">
+      <section class="site__header__barre">
+        <label class="burger" for="chkMenu">
+          <span class="iconebgr">menu</span>
+        </label>
+      </section>
       <!-- Navigation principale (div) -->
       <?php wp_nav_menu(array(
           "menu" => "entete",
           "container" => "nav"
         )) ?>
 
-
+        
         <div class="">
           <?php get_search_form(); ?>
         </div>
