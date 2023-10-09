04d9e52f2720f948fd45e0b746898f65fc47ecde
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-10-09 16:40:02 -0400

    Commentaire pour intégrer requête média - image thumbnail

--- single.php
+++ single.php
@@ -15,6 +15,9 @@
       if (have_posts()) :
         while (have_posts()) : the_post();
           the_title('<h1 class="single_titre">', '</h1>');
+
+          /****** *À COMPLÉTER* **************************************************/
+          /* Requête média pour charger le bon thumbnail ******* ****************/
           the_post_thumbnail('large');
           the_content();
         endwhile;
