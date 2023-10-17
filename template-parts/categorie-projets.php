fc2c46829e34fe0fa48fee0e619f8d938c089608
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-10-16 20:04:28 -0400

    Verifie s'il y a un thumbnail, n'affiche plus les articles de cours

--- template-parts/categorie-projets.php
+++ template-parts/categorie-projets.php
@@ -4,15 +4,15 @@
  */
     $titre = get_the_title();
   ?>
-  <h1>PROJETS</h1>
+  
 <article class="">
   <?php  if(has_post_thumbnail()) {
-    the_post_thumbnail('thumbnail');
-  } 
-?>
-  <h3><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h3>
+    the_post_thumbnail('thumbnail'); ?>
+
+    <h3><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h3>
     <?php $lien = get_permalink(); ?>
     <?php $lire = "<span><a href='" . $lien . "'>... &#187;</a></span>" ?>
    <p> <?= wp_trim_words(get_the_excerpt(), 10, $lire) ?> </p>
-  
+<?php  } 
+?>
 </article>
