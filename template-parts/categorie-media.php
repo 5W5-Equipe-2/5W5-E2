b7c205b7ccc8ad8141adc4ccdd60bb6dc822415a
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-10-31 23:10:06 -0400

    Affiche une image par défaut si aucune image trouvée

--- template-parts/categorie-media.php
+++ template-parts/categorie-media.php
@@ -9,7 +9,6 @@
 
 $idx = 0;
 if (have_posts()) : while (have_posts()) : the_post();
-
     if (in_category('media')) {
         $photo = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'larger');
         $featuredText = get_post_meta($post->ID, '_featuredText', true);
@@ -20,7 +19,6 @@ if (have_posts()) : while (have_posts()) : the_post();
             <article id="post-<?php the_ID(); ?>">
                 <div>
                     <img src="<?= $photo[0]; ?>" width="<?= $photo[1]; ?>" height="<?= $photo[2]; ?>" />
-                    <a href="<?php the_permalink() ?>"><?= $featuredTextBold ?> <small><?= $featuredText ?></small> <span>&#10142;</span></a>
                 </div>
             </article>
             <?php
@@ -29,6 +27,14 @@ if (have_posts()) : while (have_posts()) : the_post();
     }
 endwhile;
 endif;
+
+// Si aucun article n'a été trouvé, affichez l'image de remplacement
+if ($idx === 0) {
+    $imagePath = get_template_directory_uri() . '/images/media_vedette_test.jpg';
+    ?>
+    <img src="<?php echo $imagePath; ?>" alt="Image vedette">
+    <?php
+}
 
 ?>
 
